0d154cc8770ff78ecaa0ce66e1434c69e32fff2b
vincenzovolpe <vinci792010@gmail.com>
2019-11-17 19:04:28 +0100

    Aggiunta logica messaggi

--- main.js
+++ main.js
@@ -21,34 +21,40 @@ else {
 // Versione avanzata del gioco dei dadi: Utente contro PC
 var risposta = true;
 while (risposta) {
+    // Resetto i dadi e i messaggi visualizzati
+    document.getElementById('mostra-dado-utente').setAttribute("class", "notvisible");
+    document.getElementById('mostra-dado-pc').setAttribute("class", "notvisible");
+    document.getElementById('risultato').setAttribute("class", "notvisible");
     // Mostriamo titolo del gioco
     document.getElementById("titolo").innerHTML = "LANCIO DEI DADI";
 
     // Messaggio al cliente per lanciare il dado
-    alert('CLICCA SU [OK] PER LANCIARE IL TUO DADO');
+    alert('LANCIA IL TUO DADO');
 
     // Tiriamo il dado per l'utente con una funzione random
     var dadoutente = (Math.floor(Math.random() * 6) + 1);
 
     // Stampiamo il punteggio del dado dell' utente
-    //document.getElementById("mostra-dado-utente").setAttribute("class", "visibile");
-    document.getElementById("utente").innerHTML = "Il tuo punteggio è:    ";
+    document.getElementById('mostra-dado-utente').setAttribute("class", "visible");
+    document.getElementById("utente").innerHTML = "Tu";
     document.getElementById("imgdadoutente").src = "img/" + dadoutente + ".png";
 
 
-    // Messaggio al cliente per lanciare il dado
-    alert("CLICCA SU [OK] PER FAR LANCIARE IL DADO AL PC");
+    // Messaggio al cliente per far lanciare il dado al computer
+    alert("FAI LANCIARE IL DADO AL COMPUTER");
     // Tiriamo il dado per il PC con una funzione random
     var dadopc = Math.floor(Math.random() * 6) + 1;
 
     // Stampiamo il punteggio del dado dell' utente
     //document.getElementById("mostra-dado-pc").setAttribute("class", "visibile");
-    document.getElementById("pc").innerHTML = "Il punteggio del pc è: ";
+    document.getElementById('mostra-dado-pc').setAttribute("class", "visible");
+    document.getElementById("pc").innerHTML = "Computer";
     document.getElementById("imgdadopc").src = "img/" + dadopc + "pc.png";
 
     // Confrontiamo il punteggio dell'utente con quello del PC
+    document.getElementById('risultato').setAttribute("class", "visible");
     if (dadoutente > dadopc) {
-        document.getElementById("risultato").innerHTML = "Complimenti hai vinto!!!"
+        document.getElementById("risultato").innerHTML = "Complimenti hai vinto!!!";
         document.getElementById("risultato").style.color = "green";
     }
     else {
@@ -58,3 +64,5 @@ while (risposta) {
     }
     risposta = confirm('VUOI GIOCARE ANCORA?');
 }
+document.getElementById('risultato').innerHTML = "Sei uscito dal gioco, torna quando vuoi!!!";
+document.getElementById("risultato").style.color = "purple";
